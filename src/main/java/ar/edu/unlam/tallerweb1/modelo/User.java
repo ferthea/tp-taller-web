da676e4a1be50abeff7ca269c8373e9724157eb2
Fernando Thea <fernando.thea@mercadolibre.com>
2017-06-02 12:55:45 -0300

    wip

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/User.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/User.java
@@ -1,9 +1,6 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
