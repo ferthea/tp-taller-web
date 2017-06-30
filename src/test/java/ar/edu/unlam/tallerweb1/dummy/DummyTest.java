d3b656e6ca088cdb67e418971034ee3feae72a83
Fernando Thea <fernando.thea@mercadolibre.com>
2017-06-29 22:57:52 -0300

    travis..

---
+++ src/test/java/ar/edu/unlam/tallerweb1/dummy/DummyTest.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.tallerweb1.dummy;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class DummyTest {
+
+    @Test
+    public void DummyTest(){
+        Integer numero = 2;
+        assertTrue(numero == 2);
+    }
+}
